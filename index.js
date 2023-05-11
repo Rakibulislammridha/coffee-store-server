7639809127850802094b8c1fa3f5a488bfdc8b52
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-05-11 12:15:03 +0600

    get data from backend and send it to client

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.use(express.json());
 // console.log(process.env.DB_USER);
 // console.log(process.env.DB_PASS);
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ztqljx.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-iu5stap-shard-00-00.3ztqljx.mongodb.net:27017,ac-iu5stap-shard-00-01.3ztqljx.mongodb.net:27017,ac-iu5stap-shard-00-02.3ztqljx.mongodb.net:27017/?ssl=true&replicaSet=atlas-6ti1ov-shard-0&authSource=admin&retryWrites=true&w=majority`
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ztqljx.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,12 +32,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee')
+
+    app.get('/coffee', async(req, res)=>{
+        const cursor = coffeeCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/coffee', async(req, res)=>{
+        const newCoffee = req.body;
+        console.log(newCoffee);
+        const result = await coffeeCollection.insertOne(newCoffee)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
